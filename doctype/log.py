aa67b757b3940dcfdb7e48d3a5f7d01e87cd4317
cwoebker <cwoebker@gmail.com>
2011-07-30 01:16:45 +0200

    updated plugins for future plugin system #5

---
+++ doctype/log.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import sys
+import os
+
+__author__ = 'cwoebker'
+
+dirname = os.path.dirname(os.path.abspath(__file__))
+up_dir = os.path.dirname(dirname)
+sys.path.append(up_dir)
+
+from general.interfaces import DocType
+from plugnplay import Plugin, man
+
+class LOG(Plugin):
+    implements = [DocType]
+
+    def load(self, path):
+        self.fobj = open(path, "r")
+        self.content = ""
+        for line in self.fobj:
+            self.content += line
+        self.fobj.close()
+
+    def search(self, key):
+        print self.content
+        return "OK"
