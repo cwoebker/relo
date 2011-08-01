98ed1c307180b9e4cc88bd9cb916b9fb46c3bf27
Cecil Woebker <me@cwoebker.com>
2011-08-01 19:54:59 +0200

    bumped version number

--- general/__init__.py
+++ general/__init__.py
@@ -4,7 +4,7 @@ import doctype
 __all__ = ['manage', 'interfaces']
 
 
-VERSION = (0, 1, 0, 'alpha')
+VERSION = (0, 2, 0, 'alpha')
 
 def get_version():
     version = '%s.%s' % (VERSION[0], VERSION[1])
