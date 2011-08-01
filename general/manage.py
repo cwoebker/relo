352c374230ffc362f50eb0b14fcef3e8b3ad628f
Cecil Woebker <me@cwoebker.com>
2011-08-01 23:23:03 +0200

    some improves & beginning of #14

--- general/manage.py
+++ general/manage.py
@@ -9,11 +9,9 @@ import general
 from doctype import *
 
 class Manager(object):
-    def __init__(self, key):
+    def __init__(self, key, extList):
         self.key = key
         plugins = 'doctype'
-        if not plugins:
-            return
         print "Collecting plugins..."
         files = glob(os.path.join(plugins, '*.py'))
         files.remove(plugins + '/__init__.py')
