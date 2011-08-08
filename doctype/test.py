4af317a8d1e7e5f7eda443254f754bc0a4101e60
cwoebker <cwoebker@gmail.com>
2011-08-08 22:05:06 +0200

    added testing plugin

---
+++ doctype/test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import sys
+import os
+
+dirname = os.path.dirname(os.path.abspath(__file__))
+up_dir = os.path.dirname(dirname)
+sys.path.append(up_dir)
+
+from general.interfaces import DocType
+
+import re
+
+class TEST(DocType):
+    name = "TEST Plugin"
+
+    def load(self, path):
+        self.path = path
+        self.fobj = open(path, "r")
+        self.content = ""
+        for line in self.fobj:
+            self.content += line
+        self.fobj.close()
+
+    def search(self, key):
+        if not (re.search(key, self.content) == None):
+            for m in re.finditer(key, self.content):
+                print "Found at position: " + str(m.start())
+        else:
+            print "Nothing found"
+
+        print "Finished with: " + self.path
