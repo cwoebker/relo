fd7363ad79d7a33571e2339b92546abdd5090f11
Cecil Woebker <me@cwoebker.com>
2011-08-10 15:15:06 +0200

    everything works, renamed relo.py to relopy

--- relo/__init__.py
+++ relo/__init__.py
@@ -6,7 +6,7 @@ def get_version():
     return core.get_version()
 
 class Relo:
-    def __init__(self, _debug=False, _hidden=False, _type=False, _recursive=False, _directory='./', _key=''):
+    def __init__(self, _debug=False, _hidden=False, _content=False, _recursive=False, _directory='./', _key=''):
         """
         Main Relo class
         """
@@ -16,11 +16,16 @@ class Relo:
         self.dir = _directory
         self.key = _key
 
+        if _content:
+            self.type = "content Search"
+        else:
+            self.type = "fileName Search"
+
         print "Relo: version %s" % get_version()
         print "Verbose: " + str(bool(self.verbose))
         print "Hidden Files: " + str(bool(self.hidden))
         print "Recursive: " + str(bool(self.recursive))
-        print "Search Type: " + _type
+        print "Search Type: " + self.type
         print "Directory: " + self.dir
         print "Searching for: " + self.key
 
@@ -46,9 +51,13 @@ class Relo:
                 self.extList.append(item)
 
     def start(self):
+        if 'content' in self.type:
+            self.startContent()
+        else:
+            self.startName()
+    def startContent(self):
         manager = manage.Manager(self.key, self.extList)
         for item in self.filteredList:
             manager.start(item)
-
     def startName(self):
         core.fileNameSearch(self.fileList, self.key)
