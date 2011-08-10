66ae7379cb96a3740862d41d0c202a10c52d14c1
Cecil Woebker <me@cwoebker.com>
2011-08-10 10:00:10 +0200

    moved relo class intro relo library

--- relo.py
+++ relo.py
@@ -2,63 +2,14 @@
 
 import sys
 import argparse
-from relo import doctype
-from relo import core
-from relo.core import manage
+import relo
+from relo import Relo
 from relo import yapsy
 
 __author__ = "cwoebker"
-__version__ = core.get_version()
+__version__ = relo.get_version()
 __copyright__ = "cwoebker"
 __license__ = "See in LICENSE file"
-
-class Relo:
-    def __init__(self, _debug, _hidden, _type, _recursive, _directory, _key):
-        """
-        Main Relo class
-        """
-        self.verbose = _debug
-        self.hidden = _hidden
-        self.recursive = _recursive
-        self.dir = _directory
-        self.key = _key
-        
-        print "Relo: version %s" % __version__
-        print "Verbose: " + str(bool(self.verbose))
-        print "Hidden Files: " + str(bool(self.hidden))
-        print "Recursive: " + str(bool(self.recursive))
-        print "Search Type: " + _type
-        print "Directory: " + self.dir
-        print "Searching for: " + self.key
-
-        self.filteredList = []
-        self.extList = []
-        self.total_size = 0
-        self.fileList = []
-
-    def list(self):
-        if self.recursive:
-            print "Listing directory content recursively..."
-            self.total_size, self.fileList = core.recursiveListFiles(self.dir, self.hidden)
-        else:
-            print "Listing directory content..."
-            self.total_size, self.fileList = core.listFiles(self.dir, self.hidden)
-        print "Supported File Types: " + repr(doctype.__all__)
-
-    def filter(self):
-        self.filteredList = core.filterList(self.fileList)
-        for itempath in self.filteredList:
-            item = core.getFileType(itempath)
-            if item not in self.extList:
-                self.extList.append(item)
-
-    def start(self):
-        manager = manage.Manager(self.key, self.extList)
-        for item in self.filteredList:
-            manager.start(item)
-
-    def startName(self):
-        core.fileNameSearch(self.fileList, self.key)
 
 def main(argv):
     """
