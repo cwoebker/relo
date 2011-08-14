73f81281a76d2f9629fa005a7edb3d1f922d6271
cwoebker <cwoebker@gmail.com>
2011-08-14 20:29:08 +0200

    added default plugin for all option

---
+++ relo/doctype/default.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import sys
+import os
+
+dirname = os.path.dirname(os.path.abspath(__file__))
+up_dir = os.path.dirname(dirname)
+sys.path.append(up_dir)
+
+from relo.core.interfaces import DocType
+
+import re
+
+class TXT(DocType):
+    name = "DEFAULT Plugin"
+
+    def load(self, path):
+        self.path = path
+        self.fobj = open(path, "r")
+        self.content = ""
+        for line in self.fobj:
+            self.content += line
+        self.fobj.close()
