f96f07b40862d30ac90837516e49a7958ec02585
cwoebker <cwoebker@gmail.com>
2011-12-18 22:52:55 +0100

    custom logger added

--- relo/core/log.py
+++ relo/core/log.py
@@ -1,6 +1,9 @@
-__name__ = "relo.log"
+#!/usr/bin/env python
+# encoding: utf-8
 
-import logging, logging.config
+import sys
+
+"""import logging, logging.config
 
 
 
@@ -63,4 +66,47 @@ def setup_log(name, info, debug, filelog):
         ch.setLevel(logging.WARNING)
     cmdFormatter = reloFormatter(COLOR_CONSOLE_FORMAT)
     ch.setFormatter(cmdFormatter)
-    reloLog.addHandler(ch)
+    reloLog.addHandler(ch)"""
+
+
+class Color(object):
+    DEBUG = '\033[35m'
+    INFO = '\033[32em'
+    ERROR = '\033[33[31m'
+    ENDC = '\033[0m'
+
+    @classmethod
+    def _deco(cls, msg, color):
+        return '%s%s%s' % (color, msg, cls.ENDC)
+
+    @classmethod
+    def debug(cls, msg):
+        return cls._deco(msg, cls.DEBUG)
+    @classmethod
+    def info(cls, msg):
+        return cls._deco(msg, cls.INFO)
+    @classmethod
+    def error(cls, msg):
+        return cls._deco(msg, cls.ERROR)
+
+class Logger(object):
+    def debug(self, msg):
+        self._stdout(Color.debug("DEBUG: %s\n" % msg))
+
+    def log(self, msg):
+        self._stdout("%s\n" % (msg))
+
+    def info(self, msg):
+        self._stdout(Color.info('%s\n' % msg))
+
+    def error(self, msg):
+        self._stderr(Color.error("ERROR: %s\n" % msg))
+
+    def _stdout(self, msg):
+        sys.stdout.write(msg)
+        sys.stdout.flush()
+    def _stderr(self, msg):
+        sys.stderr.write(msg)
+        sys.stderr.flush()
+
+logger = Logger()
