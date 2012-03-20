302e08a929dced51ea5c71a378e2b537fe76cce5
cwoebker <cwoebker@gmail.com>
2012-03-20 08:46:52 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
-from setuptools import setup, find_packages
-from distutils.command.install import INSTALL_SCHEMES
+import sys
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+APP_NAME = 'Relo'
+APP_SCRIPT = './relopy'
+VERSION = '0.7.0'
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-setup(
-    name = "Relo",
-    version = '0.7-beta',
+# Grab requirements.
+with open('requirements.txt') as f:
+    required = f.readlines()
+
+settings = dict()
+
+# Publish Helper.
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+
+# Build Helper.
+if sys.argv[-1] == 'build':
+    try:
+        import py2exe
+    except ImportError:
+        print 'py2exe is required to continue.'
+        sys.exit(1)
+
+    sys.argv.append('py2exe')
+
+    settings.update(
+        console=[{'script': APP_SCRIPT}],
+        zipfile = None,
+        options = {
+            'py2exe': {
+                'compressed': 1,
+                'optimize': 0,
+                'bundle_files': 1}})
+
+settings.update(
+    name = APP_NAME,
+    version = VERSION,
     author = "Cecil Woebker",
     author_email = "cwoebker@gmail.com",
-    maintainer = "Cecil Woebker",
-    maintainer_email = "cwoebker@gmail.com",
     description = ("Recursive Document Content Search in Python"),
+    long_description=open('README.rst').read(),
     keywords = "python search document content",
-    url = "http://cwoebker.com/relo",
-    packages = find_packages(),
+    url = "http://relo.cwoebker.com/",
+    packages = ['relo',],
     include_package_data=True,
+    install_requires=required,
     license='BSD',
-    long_description=read('README.rst'),
     scripts = ['relopy'],
-    install_requires=['BeautifulSoup >= 3.2.0', 'pdfminer >= 20110515', 'argparse >= 1.2.1', 'redis >= 2.4.10', 'progressbar >= 2.3'],
-    classifiers=[
+    classifiers=(
         "Development Status :: 4 - Beta",
         "Environment :: Console",
+        "Natural Language :: English",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
         "Topic :: Utilities",
@@ -37,6 +78,12 @@ setup(
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "License :: OSI Approved :: BSD License",
-    ],
+    ),
+    entry_points={
+        'console_scripts': [
+            'relo = relo:main',
+        ],
+    }
 )
 
+setup(**settings)
